17d7a2985415908315fe39c010918557b39323c1
Aditya Khowal <108611217+AdityaKhowalGithub@users.noreply.github.com>
2024-04-28 12:25:01 -0700

    Initial Commit

---
+++ iKid/iKidApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKidApp.swift
+//  iKid
+//
+//  Created by Aditya Khowal on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct iKidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
